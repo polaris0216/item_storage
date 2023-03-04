d9c68748edba5a4afb551dd0b048b7c863a70320
polaris0216 <sonwoonjun@gmail.com>
2023-03-04 21:31:44 +0900

    item managerをpolarisに登録する用

---
+++ src/main/java/com/example/form/ItemForm.java
@@ -0,0 +1,34 @@
+package com.example.form;
+
+public class ItemForm {
+
+    private String name;
+
+    private Integer price;
+
+
+    public String getName() {
+        return this.name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public Integer getPrice() {
+        return this.price;
+    }
+    public void setPrice(Integer price) {
+        this.price = price;
+    }
+
+    /* 省略 */
+
+    private Integer categoryId;
+
+    public Integer getCategoryId() {
+        return this.categoryId;
+    }
+
+    public void setCategoryId(Integer categoryId) {
+        this.categoryId = categoryId;
+    }
+}
---
+++ src/main/java/com/example/repository/ItemRepository.java
@@ -0,0 +1,18 @@
+package com.example.repository;
+/* 省略 */
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.entity.Item;
+
+@Repository
+public interface ItemRepository extends JpaRepository<Item, Integer> {
+	// SELECT * FROM ITEM WHERE DELETED_AT IS NULL
+
+	public List<Item> findByDeletedAtIsNullOrderByIdAsc();
+
+	//public List<Item> findAllByOrderByIdAsc();
+
+}
